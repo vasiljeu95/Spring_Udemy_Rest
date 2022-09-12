a01aa13ff8d8b741a6d26427f3ff0629a34aac86
vasiljeu95 <vasilyeu95@gmail.com>
2022-09-12 03:05:01 +0200

    create Global Exception Handler class for all exception

---
+++ src/main/java/com/github/vasiljeu95/exeptionHandling/EmployeeGlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.github.vasiljeu95.exeptionHandling;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ * EmployeeGlobalExceptionHandler
+ *
+ * @author Stepan Vasilyeu
+ * @since 12.09.2022
+ */
+@ControllerAdvice
+public class EmployeeGlobalExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleException (NoSuchEmployeeException exception) {
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+
+        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleException (Exception exception) {
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+
+        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
+    }
+}
